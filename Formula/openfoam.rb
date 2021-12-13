be6bc10d0c6b2a9fdca970d7e587c40a0f8d0b52
Andrew Janke <andrew@apjanke.net>
2021-12-12 20:53:00 -0500

    Add license declaration, add third-party macOS patch

--- Formula/openfoam.rb
+++ Formula/openfoam.rb
@@ -3,7 +3,7 @@ class Openfoam < Formula
   homepage "https://www.openfoam.com/"
   url "https://dl.openfoam.com/source/v2106/OpenFOAM-v2106.tgz"
   sha256 "11e41e5b9a253ef592a8f6b79f6aded623b28308192d02cec1327078523b5a37"
-  license ""
+  license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
   depends_on "boost"
@@ -16,12 +16,24 @@ class Openfoam < Formula
   depends_on "paraview"
   depends_on "scotch"
 
+  # Third-party patch to get OpenFOAM building on macOS
+  # Let's just see if this works!
+  # Will need to copy this to Homebrew to get the formula accepted in Homebrew Core.
+  # May need to get license resolved, first:
+  #   https://github.com/mrklein/openfoam-os-x/issues/74
+  patch do
+    url "https://raw.githubusercontent.com/mrklein/openfoam-os-x/master/OpenFOAM-v2106.patch"
+    sha256 "08cf5c07a7a51001c90b2857c3f633e9d30499e89af95790e6791f4007be3f5d"
+  end
+
   def install
     # The CPU count gets baked in to the source code at build time?
     # Guess something conservative.
     ENV["WM_NCOMPPROCS"] = "4"
     # The OpenFOAM build does this weird thing with a custom env setup
-    system "source ./etc/bashrc; ./Allwmake -j"
+    # Use a non-parallel (no "-j") build to get better logs, until we get the basic build working
+    system "source ./etc/bashrc; ./Allwmake"
+    # system "source ./etc/bashrc; ./Allwmake -j"
 
 
     # TODO: Probly delete everything below here
